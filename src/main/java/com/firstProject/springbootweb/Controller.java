44f8df5ecd03ad0a060096f4091f72a953d78954
Arthur <arthurrodri015@gmail.com>
2022-02-21 22:07:54 -0300

    Putting some annotations in the Controller class

---
+++ src/main/java/com/firstProject/springbootweb/Controller.java
@@ -0,0 +1,13 @@
+package com.firstProject.springbootweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String message(){
+        return "My first Spring Boot Web project";
+    }
+}
