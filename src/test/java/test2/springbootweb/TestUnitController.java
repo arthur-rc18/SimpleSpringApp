93356e437931c451fe4b28a3ca35dfdbf3b09810
Arthur <arthurrodri015@gmail.com>
2022-02-23 22:30:38 -0300

    Adding a JUnit method to the test

--- src/test/java/test2/springbootweb/TestUnitController.java
+++ src/test/java/test2/springbootweb/TestUnitController.java
@@ -1,4 +1,15 @@
 package test2.springbootweb;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestUnitController {
+
+    @Test
+    public void testUniT(){
+        TestController controller = new TestController();
+        String result = controller.hello("Test");
+        assertEquals("Welcome, Test", result);
+    }
 }
